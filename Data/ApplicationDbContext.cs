2975f78eda8e56b770d9b83bfc0cae08df2913fe
Abimbola <olaolaide@outlook.com>
2024-06-28 20:30:25 +0100

    Added Identity

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Models.AuthModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApiUser>
+{
+    public ApplicationDbContext(DbContextOptions options): base(options)
+    {
+        
+    }
+}
---
+++ Models/AuthModels/ApiUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Ecommerce.Models.AuthModels;
+
+public class ApiUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
